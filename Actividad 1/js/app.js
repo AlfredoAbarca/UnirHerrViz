0283d3439a4d14a94237d12471a6c8cada7346d5
AlfredoAbarca <aabarca@gmail.com>
2022-12-21 16:21:00 -0500

    update

--- Actividad 1/js/app.js
+++ Actividad 1/js/app.js
@@ -2,5 +2,5 @@ const body=d3.select('body')
 
 //body.style('background-color','#F00')
 const cambio = (color) => {
-    body.style('backgroud-color',color)
+    body.style('background-color',color)
 }
