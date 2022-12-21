620f3bb103bd19c90fa985d493d1e3a6ed15cee5
AlfredoAbarca <aabarca@gmail.com>
2022-12-21 16:34:39 -0500

    update

--- Actividad 1/js/app.js
+++ Actividad 1/js/app.js
@@ -1,6 +1,18 @@
 const body=d3.select('body')
+const numero=d3.select("#numero")
 
 //body.style('background-color','#F00')
+const menos = d3.select("#menos")
+const mas = d3.select("#mas")
+
 const cambio = (color) => {
     body.style('background-color',color)
 }
+
+const delta = (i) =>{
+    n += i
+    numero.html(n)
+}
+
+menos.on("click",()=>delta(-1))
+mas.on("click",()=>delta(1))
