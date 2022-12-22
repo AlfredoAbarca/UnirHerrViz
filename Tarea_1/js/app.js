528606c698c56936b55f8a87f16836532bcbb67a
AlfredoAbarca <aabarca@gmail.com>
2022-12-22 18:11:04 -0500

    f

--- Tarea_1/js/app.js
+++ Tarea_1/js/app.js
@@ -15,38 +15,7 @@ var svg = d3.select("#grafica")
 
 
 
-d3.csv("https://raw.githubusercontent.com/AlfredoAbarca/UnirHerrViz/main/Tarea_1/data/test.csv",
+d3.csv("https://raw.githubusercontent.com/AlfredoAbarca/UnirHerrViz/main/Tarea_1/data/test.csv")
   // When reading the csv, I must format variables:
-function(data){
-    return { Fecha : d3.timeParse("%Y-%m-%d")(data.Fecha), Muertes : data.Muertes }
-  },
-
-  // Now I can use this dataset:
-function(data){
-    // Add X axis --> it is a date format
-    var x = d3.scaleTime()
-      .domain(d3.extent(data, function(d) {return d.Fecha; }))
-      .range([ 0, width ]);
-    svg.append("g")
-      .attr("transform", "translate(0," + height + ")")
-      .call(d3.axisBottom(x));
-
-    // Add Y axis
-    var y = d3.scaleLinear()
-      .domain([0, d3.max(data, function(d) { return +d.Muertes; })])
-      .range([ height, 0 ]);
-    svg.append("g")
-      .call(d3.axisLeft(y));
-
-    // Add the line
-    svg.append("path")
-      .datum(data)
-      .attr("fill", "none")
-      .attr("stroke", "steelblue")
-      .attr("stroke-width", 1.5)
-      .attr("d", d3.line()
-        .x(function(d) { return x(d.Fecha) })
-        .y(function(d) { return y(d.Muertes) })
-        )
-
-})
+.row(function(d){return{ Fecha: d.Fecha, Muertes : d.Muertes}; })
+.get(function(data){console.log(data)})
