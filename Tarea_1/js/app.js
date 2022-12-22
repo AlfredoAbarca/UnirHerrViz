3c9c95b9c4193d81c46a5f30848dc87edcdf2b4d
AlfredoAbarca <aabarca@gmail.com>
2022-12-22 12:23:00 -0500

    g

--- Tarea_1/js/app.js
+++ Tarea_1/js/app.js
@@ -37,7 +37,7 @@ console.log(slices[1]);
 const xScale = d3.scaleTime().range([0,width]);
 const yScale = d3.scaleLinear().rangeRound([height, 0]);
 xScale.domain(d3.extent(data, function(d){
-    return timeConv(d.date)}));
+    return timeConv(d.Fecha)}));
 yScale.domain([(0), d3.max(slices, function(c) {
     return d3.max(c.values, function(d) {
         return d.measurement + 4; });
