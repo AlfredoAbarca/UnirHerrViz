347298514a0a0f0f964b8c058d115f4112b5b449
AlfredoAbarca <aabarca@gmail.com>
2022-12-22 17:32:50 -0500

    g

--- Tarea_1/js/app.js
+++ Tarea_1/js/app.js
@@ -1,69 +1,53 @@
+// set the dimensions and margins of the graph
+var margin = {top: 10, right: 30, bottom: 30, left: 60},
+    width = 460 - margin.left - margin.right,
+    height = 400 - margin.top - margin.bottom;
 
-var parseDate = d3.timeParse("%m/%d/%Y");
-
-var margin = {left: 50, right: 20, top: 20, bottom: 50 };
-
-var width = 960 - margin.left - margin.right;
-var height = 500 - margin.top - margin.bottom;
-
-
-var max = 0;
-
-var xNudge = 50;
-var yNudge = 20;
-
-var minDate = new Date();
-var maxDate = new Date();
+// append the svg object to the body of the page
+var svg = d3.select("#grafica")
+  .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform",
+          "translate(" + margin.left + "," + margin.top + ")");
 
 
 
 
 d3.csv("https://raw.githubusercontent.com/AlfredoAbarca/UnirHerrViz/main/Tarea_1/data/All_MX_Covid_Sumarized.csv")
-    .row(function(d) { return { Fecha: parseDate(d.Fecha), Casos_Confirmados: Number(d.Casos_Confirmados.trim().slice(1))}; })
-    .get(function(error, rows) {
-	    max = d3.max(rows, function(d) { return d.Casos_Confirmados; });
-	    minDate = d3.min(rows, function(d) {return d.Fecha; });
-		maxDate = d3.max(rows, function(d) { return d.Fecha; });
+  // When reading the csv, I must format variables:
+  function(d){
+    return { Fecha : d3.timeParse("%Y-%m-%d")(d.Fecha), Casos_Confirmados : d.Casos_Confirmados }
+  },
 
+  // Now I can use this dataset:
+  function(data) {
 
-		var y = d3.scaleLinear()
-					.domain([0,max])
-					.range([height,0]);
+    // Add X axis --> it is a date format
+    var x = d3.scaleTime()
+      .domain(d3.extent(data, function(d) { return d.Fecha; }))
+      .range([ 0, width ]);
+    svg.append("g")
+      .attr("transform", "translate(0," + height + ")")
+      .call(d3.axisBottom(x));
 
-		var x = d3.scaleTime()
-					.domain([minDate,maxDate])
-					.range([0,width]);
+    // Add Y axis
+    var y = d3.scaleLinear()
+      .domain([0, d3.max(data, function(d) { return +d.Casos_Confirmados; })])
+      .range([ height, 0 ]);
+    svg.append("g")
+      .call(d3.axisLeft(y));
 
-		var yAxis = d3.axisLeft(y);
+    // Add the line
+    svg.append("path")
+      .datum(data)
+      .attr("fill", "none")
+      .attr("stroke", "steelblue")
+      .attr("stroke-width", 1.5)
+      .attr("d", d3.line()
+        .x(function(d) { return x(d.Fecha) })
+        .y(function(d) { return y(d.Casos_Confirmados) })
+        )
 
-		var xAxis = d3.axisBottom(x);
-
-		var line = d3.line()
-			.x(function(d){ return x(d.Fecha); })
-			.y(function(d){ return y(d.Casos_Confirmados); })
-			.curve(d3.curveCardinal);
-
-
-		var svg = d3.select("#grafica").append("svg").attr("id","svg").attr("height","100%").attr("width","100%");
-		var chartGroup = svg.append("g").attr("class","chartGroup").attr("transform","translate("+xNudge+","+yNudge+")");
-
-		chartGroup.append("path")
-			.attr("class","line")
-			.attr("d",function(d){ return line(rows); })
-
-
-		chartGroup.append("g")
-			.attr("class","axis x")
-			.attr("transform","translate(0,"+height+")")
-			.call(xAxis);
-
-		chartGroup.append("g")
-			.attr("class","axis y")
-			.call(yAxis);
-
-
-
-	});
-
-
-
+})
