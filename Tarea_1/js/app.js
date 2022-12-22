9dd40f895cb6dc6d4ae640926433ef4a9cf6d266
AlfredoAbarca <aabarca@gmail.com>
2022-12-22 12:47:09 -0500

    g

--- Tarea_1/js/app.js
+++ Tarea_1/js/app.js
@@ -45,7 +45,7 @@ dataset.then(function(data){
         
         //-------------------------Preparacion de la graduacion de los ejes para nuestra grafica------------------//
         const yaxis = d3.axisLeft()
-            .ticks((slices[1].values).length)
+            .ticks((slices[0].values).length)
             .scale(yScale);
         
         const xaxis = d3.axisBottom()
