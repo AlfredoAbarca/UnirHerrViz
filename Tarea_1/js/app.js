1d4da0797e32c40683416cddbef34ce6e1aeedc2
AlfredoAbarca <aabarca@gmail.com>
2022-12-23 03:21:41 -0500

    5

--- Tarea_1/js/app.js
+++ Tarea_1/js/app.js
@@ -4,6 +4,8 @@ var margin = {top: 10, right: 100, bottom: 50, left: 80},
     width = 800 - margin.left - margin.right,
     height = 350 - margin.top - margin.bottom;
 
+
+function Carga_Grafico_Defunciones(){
 // append the svg object to the body of the page
 var svg = d3.select("#grafica")
 .append("svg")
@@ -133,4 +135,6 @@ function mouseout() {
 
  
 
-})
+})}
+
+Carga_Grafico_Defunciones()
