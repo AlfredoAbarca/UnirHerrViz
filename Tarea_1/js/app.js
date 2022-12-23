b38ec637b4366ea756359e9ca25a1c7601f075d5
AlfredoAbarca <aabarca@gmail.com>
2022-12-23 02:34:46 -0500

    5

--- Tarea_1/js/app.js
+++ Tarea_1/js/app.js
@@ -1,6 +1,6 @@
 // set the dimensions and margins of the graph
 
-var margin = {top: 10, right: 30, bottom: 30, left: 60},
+var margin = {top: 10, right: 30, bottom: 50, left: 60},
     width = 800 - margin.left - margin.right,
     height = 600 - margin.top - margin.bottom;
 
