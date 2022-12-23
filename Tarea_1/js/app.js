e682840163f3c03f23d21100fd9c237c1f7c9c9c
AlfredoAbarca <aabarca@gmail.com>
2022-12-23 03:43:56 -0500

    5

--- Tarea_1/js/app.js
+++ Tarea_1/js/app.js
@@ -137,4 +137,97 @@ function mouseout() {
 
 })}
 
+function Carga_Grafico_Contagios(){
+    // append the svg object to the body of the page
+const svg = d3.select("#grafica_contagios")
+.append("svg")
+  .attr("width", width + margin.left + margin.right)
+  .attr("height", height + margin.top + margin.bottom)
+.append("g")
+  .attr("transform",
+        `translate(${margin.left}, ${margin.top})`);
+
+// get the data
+d3.csv("https://raw.githubusercontent.com/AlfredoAbarca/UnirHerrViz/main/Tarea_1/data/All_MX_Covid_Sumarized.csv",   function(d){
+    return { Fecha : d3.timeParse("%Y-%m-%d")(d.Fecha), Casos_Confirmados : d.Tasa_de_Contagio }
+  }).then( 
+function(data) {
+
+// X axis: scale and draw:
+const x = d3.scaleTime()
+.domain(d3.extent(data, function(d) { return d.Fecha; }))
+.range([ 0, width ]);
+svg.append("g")
+    .attr("transform", `translate(0, ${height})`)
+    .call(d3.axisBottom(x));
+
+// set the parameters for the histogram
+const histogram = d3.histogram()
+    .value(function(d) { return d.Tasa_de_Contagio; })   // I need to give the vector of value
+    .domain(x.domain())  // then the domain of the graphic
+    .thresholds(x.ticks(100)); // then the numbers of bins
+
+// And apply this function to data to get the bins
+const bins = histogram(data);
+
+// Y axis: scale and draw:
+const y = d3.scaleLinear()
+    .range([height, 0]);
+    y.domain([0, d3.max(bins, function(d) { return d.length; })]);   // d3.hist has to be called before the Y axis obviously
+svg.append("g")
+    .call(d3.axisLeft(y));
+
+// Add a tooltip div. Here I define the general feature of the tooltip: stuff that do not depend on the data point.
+// Its opacity is set to 0: we don't see it by default.
+const tooltip = d3.select("#grafica_contagios")
+  .append("div")
+  .style("opacity", 0)
+  .attr("class", "tooltip")
+  .style("background-color", "black")
+  .style("color", "white")
+  .style("border-radius", "5px")
+  .style("padding", "10px")
+
+// A function that change this tooltip when the user hover a point.
+// Its opacity is set to 1: we can now see it. Plus it set the text and position of tooltip depending on the datapoint (d)
+const showTooltip = function(event,d) {
+  tooltip
+    .transition()
+    .duration(100)
+    .style("opacity", 1)
+  tooltip
+    .html("Range: " + d.x0 + " - " + d.x1)
+    .style("left", (event.x)/2-100 + "px")
+    .style("top", (event.y)/2 + "px")
+}
+const moveTooltip = function(event,d) {
+  tooltip
+  .style("left", (event.x)/2-100 + "px")
+  .style("top", (event.y)/2 + "px")
+}
+// A function that change this tooltip when the leaves a point: just need to set opacity to 0 again
+const hideTooltip = function(event,d) {
+  tooltip
+    .transition()
+    .duration(100)
+    .style("opacity", 0)
+}
+
+// append the bar rectangles to the svg element
+svg.selectAll("rect")
+    .data(bins)
+    .join("rect")
+      .attr("x", 1)
+      .attr("transform", function(d) { return `translate(${x(d.x0)}, ${y(d.length)})`})
+      .attr("width", function(d) { return x(d.x1) - x(d.x0) -1 ; })
+      .attr("height", function(d) { return height - y(d.length); })
+      .style("fill", "#69b3a2")
+      // Show tooltip on hover
+      .on("mouseover", showTooltip )
+      .on("mousemove", moveTooltip )
+      .on("mouseleave", hideTooltip )
+
+});
+}
+Carga_Grafico_Contagios()
 Carga_Grafico_Defunciones()
