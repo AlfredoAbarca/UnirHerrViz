c73e4b4c307d21a81558742f933bbfe9f326b432
AlfredoAbarca <aabarca@gmail.com>
2022-12-23 00:53:04 -0500

    d

--- Tarea_1/js/app.js
+++ Tarea_1/js/app.js
@@ -89,7 +89,7 @@ function mouseover() {
 
 function mousemove() {
  // recover coordinate we need
- var x0 = x.invert(d3.pointer(this)[0]);
+ var x0 = d3.pointer(this)[0];
  var i = bisect(data, x0, 1);
  selectedData = data[i]
  focus
