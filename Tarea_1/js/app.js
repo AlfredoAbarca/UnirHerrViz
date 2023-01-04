f47a11610f07bd88dd5788001462efa01a516378
AlfredoAbarca <aabarca@gmail.com>
2023-01-04 18:55:05 -0500

    Updated Function

--- Tarea_1/js/app.js
+++ Tarea_1/js/app.js
@@ -1044,7 +1044,7 @@ function Create_Html_Table(HtmlDiv_id, data, columns) {
 	  .data(function (row) {
 	    return columns.map(function (column) {
         if(column == 'Fecha') {
-          row[column] = d3.timepParse("%x")(row[column]);
+          row[column] = (row[column]);
         } 
 	      return {column: column, value: row[column]};
 	    });
